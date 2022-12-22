84a72e164649f13ace917e3ade55bff928c1dc6a
Maga Umarov <magaumarov@MacBook-Air-Maga.local>
2022-12-22 22:43:18 +0300

    first commit

---
+++ src/Todos.js
@@ -0,0 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+
+function Todos() {
+    const dispatch = useDispatch()
+
+    const todos = useSelector(state => state)
+
+    const handleDelete = () =>{
+        dispatch({
+            type: 'delete',
+            payload: 'index'
+        })
+    }
+    return (
+        <ul className="list-group">
+            {todos.map((item, index) => {
+                return(
+                    <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
+                        <div>
+                            <input type='checkbox' checked={item.done}/>
+                        </div>
+                        <div>
+                            {item.text}
+                        </div>
+                        <div>
+                            <button className="btn btn-outline-danger" 
+                            onClick={() => handleDelete(index)}
+                            >
+                                X
+                            </button>
+                        </div>
+                    </li>
+                )
+            })}
+        </ul>
+    );
+  }
+  
+  export default Todos;
